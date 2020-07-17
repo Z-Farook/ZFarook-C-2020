b45361e0cd6c83d462b086b79bd015b00fc1a85d
Zahid <0950085@hr.nl>
2020-07-17 13:54:41 +0200

    Variables leaning done!

---
+++ Chapter2/Chapter2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            variablesBasics.someVariablePrinting();
+        }
+    }
+}
+
+
