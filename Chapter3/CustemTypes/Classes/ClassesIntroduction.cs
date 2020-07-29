5df646c4e031d0ed0b733327b14639bef1b8c066
Zahid <0950085@hr.nl>
2020-07-29 19:06:35 +0200

    Basics of Classes and Strucs

---
+++ Chapter3/CustemTypes/Classes/ClassesIntroduction.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    internal class ClassesIntroduction
+    {
+       /*Fields*/
+        public const string LearningMode = "Happy mode!";
+
+        /*Properties; they are kind of methods and this is the special syntax for getter and setter
+        we can also define getter and setter by ourself!
+        */
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        /*Custom getter and setter example*/
+        private int myVar;
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public ClassesIntroduction()
+        {
+        }
+
+        public ClassesIntroduction(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    internal struct PersonPhone
+    {
+        private int _phoneNumber;
+        private string _country;
+
+        public PersonPhone(int phoneNumber, string country)
+        {
+            _phoneNumber = phoneNumber;
+            _country = country;
+        }
+
+        public static object test(PersonPhone personPhone)
+        {
+            return $"Person 1:\n {personPhone._country}  {personPhone._phoneNumber}";
+        }
+    }
+
+    internal struct PersonPhone2
+    {
+        public const string LearningMode = "Happy mode!";
+        public string _country { get; set; }
+        public int _phoneNumber { get; set; }
+
+        public static Func<object, object, object, object> CountryAndPhone = (object contry, object pnum, object lMode) =>
+        {
+            var res = $"Person 2:\n{contry} {pnum} {lMode}";
+            Console.WriteLine(res);
+            return res;
+        };
+    }
+}
--- Chapter3/CustemTypes/Classes/Program.cs
+++ Chapter3/CustemTypes/Classes/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Security;
 
 namespace Classes
 {
     class Program
     {
+#pragma warning disable
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Action<object> write = Console.WriteLine;
+            write("");
+            #region For C# classes and Struct
+#if true
+            /*Instantiating a object*/
+            ClassesIntroduction person = new ClassesIntroduction();
+            /*Accessing the fields of a class */
+            person.FirstName = "Zahid";
+            person.LastName = "Farook";
+            write((person.FirstName, person.LastName));
+
+            ClassesIntroduction person1 = new ClassesIntroduction();
+            /*Accessing the fields of a class */
+            person1.FirstName = "John";
+            person1.LastName = "Doe";
+            write(person1.LastName);
+            write("");
+
+            /*Get the constant filed from cLASS*/
+            var personMode = ClassesIntroduction.LearningMode;
+
+            PersonPhone pNumber1 = new PersonPhone(12453, "Belgium");
+            write(PersonPhone.test(pNumber1));
+            write("");
+
+            PersonPhone2 pNumber2 = new PersonPhone2();
+            var country = pNumber2._country = "NL";
+            var pnum = pNumber2._phoneNumber = 123;
+            /*Get the constant filed from STRUCT*/
+            var lMode = PersonPhone2.LearningMode;
+            var res = PersonPhone2.CountryAndPhone(country, pnum, lMode );
+#endif
+            #endregion
+
+            write("");
         }
     }
 }
