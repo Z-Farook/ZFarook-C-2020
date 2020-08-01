0c14816f730ac98d93bf0c27d6c0472d9b0a5d26
Zahid <0950085@hr.nl>
2020-08-01 11:22:59 +0200

    More than one topic discussed!
    Using classes, Fields , Properties of all kinds and thier access modiefiers. Method overloading and invocation. Otional parameters and ...

---
+++ Chapter3/CustemTypes/Classes/AnonymousTypesAndMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    internal class AnonymousTypesAndMethods
+    {
+        public string FName { get; set; }
+        public string MName { get; set; }
+        public string LName { get; set; }
+    }
+    internal class WorkingWithMethod
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+
+        public void MethodOne(WorkingWithMethod wm)
+        {
+            wm.MyProperty = 101;
+            Console.WriteLine("MyProperty1: " + wm.MyProperty);
+        }
+        public void ExpressionBodiedMethod(WorkingWithMethod wm, int x) => Console.WriteLine("MyProperty2: " + (wm.MyProperty2 = x).ToString());
+    }
+    
+    //Note this class contains multiple things for different topics
+    internal class MethodOverloding
+    {
+        public static void MLoadingOne()
+        {
+            Console.WriteLine("No parameter in this method");
+        }
+
+        public static void MLoadingTwo(int n)
+        {
+            Console.WriteLine("This method contains one parameter: " + n);
+        }
+        public static void MLoadingThree(int g, int q)
+        {
+            Console.WriteLine("This method contains two parameter: " + g + " and: " + q);
+        }
+
+        public static void MForNamedArgInvocation(int l, int m)
+        {
+
+            Console.WriteLine($"L is : {l} and M is: {m}");
+        }
+
+        public static void MWithOptionalParameter(int u , int t =2 )
+        {
+
+            Console.WriteLine($"Square of u: {u} and t: {t} is: {u* t}");
+        }
+    }
+
+}
--- Chapter3/CustemTypes/Classes/Program.cs
+++ Chapter3/CustemTypes/Classes/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Security;
+using System.Numerics;
 
 namespace Classes
 {
@@ -11,7 +11,7 @@ namespace Classes
             Action<object> write = Console.WriteLine;
             write("");
             #region For C# classes and Struct
-#if true
+#if false
             /*Instantiating a object*/
             ClassesIntroduction person = new ClassesIntroduction();
             /*Accessing the fields of a class */
@@ -40,7 +40,6 @@ namespace Classes
             var lMode = PersonPhone2.LearningMode;
             var res = PersonPhone2.CountryAndPhone(country, pnum, lMode);
 
-#endif
             /*Makes the static constructor execute */
             ClassWithReadOnlyFields.getTheReadOnlyField();
 
@@ -48,8 +47,101 @@ namespace Classes
             ClassWithReadOnlyFields classWithReadOnlyFields = new ClassWithReadOnlyFields(500);
             /*printing the value of the assignment above*/
             ClassWithReadOnlyFields.TheReadOnlyFieldDefinedTroughConstructor(classWithReadOnlyFields);
+#endif
+            #endregion
+
+            #region For working with C Sharp class properties 
+#if false
+            Properties myP = new Properties();
+            myP.PlaneName = "PIA";
+            myP.TicketPrice = 120;
+            myP.PlaneName2 = "KLM";
+            myP.TicketPrice2 = 110;
+            /*print trough getter what is assigned*/
+            var res = myP.PlaneName;
+            var res2 = myP.PlaneName2;
+
+            var exBodiedP = new ExpressionBodiedProps();
+            exBodiedP.X = 4;
+            exBodiedP.Y = 4;
+            write(("Expression bodied props: ", exBodiedP.X, exBodiedP.Y));
+
+            var autoImProp = new AutoImplimentedProps();
+            autoImProp.Name = "Zahid";
+            autoImProp.LName = "Farook";
+            write(autoImProp.Name);
+            /*get the value of property initializer */
+            write(autoImProp.AuotPropInitializer);
+
+            PropertyAccessLevel x = new PropertyAccessLevel(12);
+            MakeReadOnlyOmitingTheSetter makeReadOnly = new MakeReadOnlyOmitingTheSetter("Good day");
+            write(makeReadOnly.J);
+            ExplicitReadOnlyPropTroughConstructor explicitReadOnlyProp = new ExplicitReadOnlyPropTroughConstructor(45);
+            //explicitReadOnlyProp.ID = 12; /*This will give an error since the prop is made read only*/
+
+            Person person = new Person("Zahid", "FARook");
+           // person.FirstName = ""; /*You can't do that since you are working with read only fields and they can only be assigned trough constructor*/
+            write(person.FullName);
+#endif
+            #endregion
+
+            #region For C# Anonymous types
+#if false
+            var john = new
+            {
+                Age = 68,
+                height = 4.5,
+                weight = 50
+            };
+            //john.Age = 65; /*Note here the value cannot be assigned since the anonymous types are read only */
+            write(john.Age);
+
+            //making an anonymous type using the props of an already existing object
+            AnonymousTypesAndMethods singer = new AnonymousTypesAndMethods();
+            var dName = "A";
+            singer.FName = dName;
+
+            var dancer = new
+            {
+                /*Note here the value cannot be assigned since the anonymous types are read only */
+                singer.FName,
+                singer.MName,
+                singer.LName,
+            };
+            write(dancer.FName);
+            var m1 = new WorkingWithMethod();
+            m1.MethodOne(m1);
+            m1.ExpressionBodiedMethod(m1, 10);
+
+#endif
+
+            #region For C# method overloading and invocation
+#if true
+            //MethodOverloding.MLoadingOne();
+            //MethodOverloding.MLoadingTwo(1);
+            //MethodOverloding.MLoadingThree(12, 23);
+            ///*This will be assigned as the parameters are defined in the function so: l= 2 m12*/
+            //MethodOverloding.MForNamedArgInvocation(2, 12);
+
+            ///*This will be HOWEVER ALSO ASSIGNED AS THE PARAMETERS are defined in the function
+            //  so regardless the order given here: m: 2, l: 12*/
+            //MethodOverloding.MForNamedArgInvocation(m: 2, l: 12);
+            ///*non-trailing named arguments are only allowed when the name and the position 
+            // * result in finding the same corresponding parameter. In other words if we omit the name of some parameters then
+            // the ORDER IN THE INVOCATION MUST MATCH WITH THAT OF ORIGINAL FOUND IN THE DEFINITION of the method*/
+            //MethodOverloding.MForNamedArgInvocation(l: 32, 113);
+
+            MethodOverloding.MWithOptionalParameter(2, 3);
+            MethodOverloding.MWithOptionalParameter(4);
+
+
+
+
+#endif
+            #endregion
 
             #endregion
+
             write("");
         }
     }
