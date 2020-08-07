7c5599fb8aeda3bcae412519d5a9650f28f4eca1
Zahid <0950085@hr.nl>
2020-08-07 14:34:12 +0200

    Initial
    Object-Oriented Programming with C#

---
+++ Chapter4/Object-Oriented-Programming/Inheritance Implementation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inheritance_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
