679eb60feb4e5c5a1633fe72c689d0a8eca74ba7
Zahid <0950085@hr.nl>
2020-08-19 17:32:43 +0200

    Chapter 5 initial

---
+++ Chapter5/Generics/GenericsBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
