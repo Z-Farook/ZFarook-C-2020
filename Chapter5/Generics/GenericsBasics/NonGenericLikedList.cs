fdfc8425aff31aa9234cab9ff57ef95e858a02f8
Zahid <0950085@hr.nl>
2020-08-20 15:00:37 +0200

    LinkedList using Generics and non-generic classes

---
+++ Chapter5/Generics/GenericsBasics/NonGenericLikedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsBasics
+{
+    #region For C# Non Generic classes
+#if true
+
+    public class NonGenericListNode
+    {
+        public NonGenericListNode(object v) => Value = v;
+
+        public object Value { get; }
+
+        public NonGenericListNode Next { get; internal set; }
+        public NonGenericListNode Previous { get; internal set; }
+    }
+
+    public class NonGenericLinkedList : IEnumerable
+    {
+        public NonGenericListNode First { get; private set; }
+
+        public NonGenericListNode Last { get; private set; }
+
+        public NonGenericListNode AddLast(object obj)
+        {
+            var newNode = new NonGenericListNode(obj);
+            if (First == null)
+            {
+                First = newNode;
+                Last = First;
+            }
+            else
+            {
+                Last.Next = newNode;
+                Last = newNode;
+
+            }
+
+            return newNode;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = First;
+            while (current != null)
+            {
+                yield return current.Value; //yield statement for creating an enumerator type
+                current = current.Next;
+            }
+        }
+    }
+
+#endif
+    #endregion
+}
--- Chapter5/Generics/GenericsBasics/Program.cs
+++ Chapter5/Generics/GenericsBasics/Program.cs
@@ -22,9 +22,53 @@ namespace GenericsBasics
             object o = "Some string";
             Console.WriteLine(o.GetType()); // prints System.String
 #endif
-
+            // Non generic linked list 
+#if false
+            var linkedList = new NonGenericLinkedList();
+            linkedList.AddLast(1);
+            linkedList.AddLast(12);
+            /*linkedList.AddLast("6");*/ // this gives us error because it cannot be converted to in the for each loop
+            foreach (int item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+#endif
+            // generic linked list 
+#if false
+            var listOfints = new GenericLinkedList<int>();
+            listOfints.AddLast(21);
+            //genLink.AddLast("Test") /*Nope, i can't be added the Linked list is type safe now which is now int with this instance*/
+            foreach (int item in listOfints)
+            {
+                Console.Write(item + " ");
+            }
             Console.WriteLine();
 
+            var listOfStr = new GenericLinkedList<string>();
+            listOfStr.AddLast("hello");
+            
+            foreach (string item in listOfStr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            /**Note the class type an object you still be able to add things still unsafely */
+            var listOfObjects = new GenericLinkedList<object>();
+
+            /**Also note the first line is determining the type of all the entries. Not sure how is this behavior showing up   */
+            listOfObjects.AddLast(12);
+            listOfObjects.AddLast("TEst");
+
+            foreach (var item in listOfObjects)
+            {
+                Console.Write(item  + " ");
+            }
+#endif
+
+            Console.WriteLine();
+ 
         }
     }
 }
