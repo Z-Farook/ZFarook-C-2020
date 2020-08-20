073b29ea7acd2b29972498396d1526d602a040b4
Zahid <0950085@hr.nl>
2020-08-20 11:47:56 +0200

    Basic Overview done

---
+++ Chapter5/Generics/GenericsBasics/BasicaOfGenerics.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using System.Text;
+
+namespace GenericsBasics
+{
+    #region For C# ArrayList and understanding of the boxing and unboxing
+#if true
+    public static class TestClassForBoxingAndUnboxing
+    {
+        public static ArrayList BoxTheValueType()
+        {
+            /*The reference type*/
+            ArrayList arrayList = new ArrayList();
+            ArrayList a = arrayList;
+            /**Invoking the method of the ArrayList class which take an argument of type object and we are passing the value type so the boxing
+             * is happening */
+            a.Add(23);
+            return a;
+        }
+        /**Unboxing is happing on the next line because the ArrayList is being read and giving us int back  */
+        public static void UnboxTheRefType() => Console.WriteLine(BoxTheValueType()[0]);
+
+        public static void BoxTheValueType2(int l)
+        {
+
+            ArrayList a = new ArrayList(); /*The reference type*/
+
+            //var g = a.Capacity; /*can be used for condition if we had given the initial capacity to the variable a */
+
+            for (int i = 0; i < l; i++)
+            {
+                /**Invoking the method of the ArrayList class which take an argument of type object and we are passing the value type so the boxing
+                 * is happening */
+                a.Add(i + 1);
+            }
+
+            /*Calls the method to print the ArrayList */
+            UnboxTheRefType2(a);
+        }
+
+        /// <summary>
+        /// The printer for unboxing
+        /// Boxing and unboxing are easy to use but have a big performance impact, especially when iterating through many items.
+        /// </summary>
+        /// <param name="ar"></param>
+        public static void UnboxTheRefType2(ArrayList ar)
+        {
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+#endif
+    #endregion
+
+    #region For C# Generic list<T> and its use
+#if true
+    /**The boxing and unboxing with OBJECTS have some performance issues that can be limited using the List<T> generic class from the Microsoft .Net*/
+    public class ClassWithGenericList
+    {
+        public static List<int> MakeAGenericList(int length)
+        {
+            List<int> aListC = new List<int>(length);
+            for (int i = 0; i < length; i++)
+            {
+                aListC.Add(i + 1);
+            }
+            PrintListT(aListC);
+            return aListC;
+        }
+
+        public static Func<List<int>, int> PrintListT = (List<int> listToIterate) =>
+         {
+             int res = 0;
+             foreach (var item in listToIterate)
+             {
+                 res = item;
+                 Console.WriteLine(item);
+             }
+             return res;
+         };
+    }
+
+    public static class ClassAddingDifferentDataToList
+    {
+        public static (List<string>, List<int>, List<ClassGenericListTest> ) AddDifferentTypeToGenericList(string s, int u, ClassGenericListTest classGenericListTest)
+        {
+            var addString = new List<string>();
+            addString.Add(s);
+            var addInt = new List<int>();
+            addInt.Add(u);
+            var addClass = new List<ClassGenericListTest>();
+            addClass.Add(classGenericListTest);
+            return (addString, addInt, addClass);
+        }
+    }
+    public class ClassGenericListTest
+    {
+
+    }
+
+#endif
+    #endregion
+
+}
--- Chapter5/Generics/GenericsBasics/Program.cs
+++ Chapter5/Generics/GenericsBasics/Program.cs
@@ -4,9 +4,27 @@ namespace GenericsBasics
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            //Basic Overview understanding 
+#if false
+            TestClassForBoxingAndUnboxing.UnboxTheRefType();
+            TestClassForBoxingAndUnboxing.BoxTheValueType2(5);
+
+            //Generic list<T> usage which ensures type safety
+            ClassWithGenericList.MakeAGenericList(6);
+            var classForListAdding = new ClassGenericListTest();
+            var res = ClassAddingDifferentDataToList.AddDifferentTypeToGenericList("The string", 12, classForListAdding);
+            Console.WriteLine(res.Item1.GetType());
+            Console.WriteLine(res.Item2.GetType());
+            Console.WriteLine(res.Item3.GetType());
+            object o = "Some string";
+            Console.WriteLine(o.GetType()); // prints System.String
+#endif
+
+            Console.WriteLine();
+
         }
     }
 }
