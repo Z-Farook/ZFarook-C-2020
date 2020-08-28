346884e7c3f895fa30dd89937cc4e279723b699b
Zahid <0950085@hr.nl>
2020-08-28 14:04:53 +0200

    Operators
    Pre- and post, checked and unchecked, Ternary

--- Chapter6/OperatorsandCasts/OperatorsBasics/Program.cs
+++ Chapter6/OperatorsandCasts/OperatorsBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace OperatorsBasics
 {
@@ -6,7 +7,26 @@ namespace OperatorsBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            #region For C#  OPERATOR
+#if false
+            /**Pre- and post increment */
+            OperatorHolder.PreAndPsotOper();
+            Console.WriteLine(OperatorHolder.TheTernaryOper(12));
+
+            /**The checked and unchecked operator */
+            OperatorHolder.TheCheckedOper();
+            OperatorHolder.TheUncheckedoper();
+#endif
+
+#if true
+
+#endif
+
+            #endregion
+
+            Console.WriteLine();
+
         }
     }
 }
