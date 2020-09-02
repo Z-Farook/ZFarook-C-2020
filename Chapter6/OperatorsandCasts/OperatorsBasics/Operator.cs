33aea546e1ac4d1a9f62a6ad2ebd00b23eb9cceb
Zahid <0950085@hr.nl>
2020-09-02 15:06:02 +0200

    Chnaged namespace
    OperatorsBasics to OperatorsAndCasts

--- Chapter6/OperatorsandCasts/OperatorsBasics/Operator.cs
+++ Chapter6/OperatorsandCasts/OperatorsBasics/Operator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text;
 
-namespace OperatorsBasics
+namespace OperatorsAndCasts
 {
     // The classes names indicates what they are possibly holding
     public class OperatorHolder
