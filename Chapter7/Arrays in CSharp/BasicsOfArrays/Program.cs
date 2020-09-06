d69a456f86b2843429ae3cc7d9a139de089bd246
Zahid <0950085@hr.nl>
2020-09-06 13:27:29 +0200

    Chapter 7 init

---
+++ Chapter7/Arrays in CSharp/BasicsOfArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicsOfArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
